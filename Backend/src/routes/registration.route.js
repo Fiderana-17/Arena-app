c96396bc719bb64237c2cf1591559d3fc8bda201
Fiderana-17 <hei.fiderana@gmail.com>
2025-12-15 07:27:54 +0300

    fix: modify the import

--- Backend/src/routes/registration.route.js
+++ Backend/src/routes/registration.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerTeamToTournament, getTournamentRegistrations } from '../controllers/registrationController.js';
+import { registerTeamToTournament, getTournamentRegistrations } from '../controllers/registration.controller.js';
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = express.Router();
