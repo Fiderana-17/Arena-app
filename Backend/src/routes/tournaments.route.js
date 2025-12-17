8eec0b1fc65fe332ca3ffc3d31d162d2e21d20f9
Fiderana-17 <hei.fiderana@gmail.com>
2025-12-17 11:14:05 +0300

    chore(tournaments): add calcutlate ranking , update  and delete functionnality

--- Backend/src/routes/tournaments.route.js
+++ Backend/src/routes/tournaments.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTournament, getTournaments, getTournament } from '../controllers/tournament.controller.js';
+import { createTournament, getTournaments, getTournament, updateTournament, deleteTournament, calculateRankings } from '../controllers/tournament.controller.js';
 import { authenticateToken, requireAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -8,7 +8,10 @@ router.use(authenticateToken);
 
 router.get('/', getTournaments);
 router.get('/:id', getTournament);
+router.get('/:id/rankings', calculateRankings); // Nouvelle route pour calculer les rankings
 
 router.post('/', requireAdmin, createTournament);
+router.put('/:id', requireAdmin, updateTournament);
+router.delete('/:id', requireAdmin, deleteTournament);
 
 export default router;
